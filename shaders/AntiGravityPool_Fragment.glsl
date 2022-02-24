d0c8d7282b9f7630138f797a1e3d6d365fc80e94
Erich Loftis <erichlof@swbell.net>
2022-02-24 16:43:13 -0600

    Update AntiGravityPool_Fragment.glsl

--- shaders/AntiGravityPool_Fragment.glsl
+++ shaders/AntiGravityPool_Fragment.glsl
@@ -305,7 +305,7 @@ vec3 CalculateRadiance(out vec3 objectNormal, out vec3 objectColor, out float ob
                 	RP = Re / P;
                 	TP = Tr / (1.0 - P);
 
-			if (rand() < P)
+			if (bounces == 0 && rand() < P)
 			{
 				mask *= RP;
 				rayDirection = reflect(rayDirection, nl); // reflect ray from surface
@@ -342,7 +342,7 @@ vec3 CalculateRadiance(out vec3 objectNormal, out vec3 objectColor, out float ob
                 	RP = Re / P;
                 	TP = Tr / (1.0 - P);
 
-			if (rand() < P)
+			if (diffuseCount == 0 && rand() < P)
 			{
 				mask *= RP;
 				rayDirection = reflect(rayDirection, nl); // reflect ray from surface
