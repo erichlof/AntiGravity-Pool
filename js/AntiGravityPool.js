4276f21ec849585efbd19ceb26e11277ddfb81a7
Erich Loftis <erichlof@swbell.net>
2025-04-25 22:32:23 -0500

    Update AntiGravityPool.js

--- js/AntiGravityPool.js
+++ js/AntiGravityPool.js
@@ -76,7 +76,7 @@ function initSceneData()
 	cameraFlightSpeed = 30;
 	
 	// pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
-	pixelRatio = mouseControl ? 0.75 : 0.75;
+	pixelRatio = mouseControl ? 1.0 : 0.75;
 
 	EPS_intersect = 0.001;
 
